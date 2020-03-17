1c8178e36e3dff7afdfeb27166ee97695ceb4e67
yhChen <kchenyuhang@gmail.com>
2020-03-17 09:55:51 +0800

    spring-learing项目创建

---
+++ ioc/src/main/java/com/soft1851/spring/ioc/app/TestHello.java
@@ -0,0 +1,22 @@
+package com.soft1851.spring.ioc.app;
+
+import com.soft1851.spring.ioc.entity.Hello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.logging.Logger;
+
+/**
+ * @Author yhChen
+ * @Description
+ * @Date 2020/3/17
+ */
+public class TestHello {
+    private Logger logger;
+
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+        Hello hello = (Hello) ac.getBean("hello");
+        hello.sayHello();
+    }
+}
---
+++ ioc/src/test/java/com/soft1851/spring/ioc/entity/HelloTest.java
@@ -0,0 +1,27 @@
+package com.soft1851.spring.ioc.entity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author yhChen
+ * @Description
+ * @Date 2020/3/17
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/beans.xml"})
+public class HelloTest {
+
+    @Autowired
+    private Hello hello;
+
+    @Test
+    public void sayHello() {
+        hello.sayHello();
+    }
+}
